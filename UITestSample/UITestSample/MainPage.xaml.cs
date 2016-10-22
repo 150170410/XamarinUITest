21d28d9fc6f8ab56d131c97db18cb89397efa4f6
parth7676 <parth7676@gmail.com>
2016-10-22 17:03:10 +0530

    Changes in UI Test Sample

---
+++ UITestSample/UITestSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UITestSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnHello.Clicked += BtnHello_Clicked;
+        }
+
+        private void BtnHello_Clicked(object sender, EventArgs e)
+        {
+            lblHello.Text = "HelloMoto";
+        }
+    }
+}
